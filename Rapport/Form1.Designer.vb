a82bd8723bc96d4dc4e23de037fdcae1b4b4475c
Skream20 <118281774+Skream20@users.noreply.github.com>
2024-10-18 11:05:58 +0200

    button

--- Rapport/Form1.Designer.vb
+++ Rapport/Form1.Designer.vb
@@ -25,10 +25,11 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
-        Me.logout = New System.Windows.Forms.Button()
-        Me.ReportButton = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.ReportButton = New System.Windows.Forms.Button()
+        Me.logout = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -42,52 +43,6 @@ Partial Class Form1
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(872, 53)
         Me.Panel1.TabIndex = 0
-        '
-        'ImageList1
-        '
-        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
-        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
-        Me.ImageList1.Images.SetKeyName(0, "images.jpg")
-        '
-        'logout
-        '
-        Me.logout.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
-        Me.logout.BackColor = System.Drawing.Color.White
-        Me.logout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.logout.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.logout.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.logout.FlatAppearance.BorderSize = 0
-        Me.logout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.logout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.logout.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.logout.ForeColor = System.Drawing.Color.Black
-        Me.logout.Location = New System.Drawing.Point(764, 15)
-        Me.logout.Name = "logout"
-        Me.logout.Size = New System.Drawing.Size(95, 23)
-        Me.logout.TabIndex = 0
-        Me.logout.Text = "Déconnexion"
-        Me.logout.UseVisualStyleBackColor = False
-        '
-        'ReportButton
-        '
-        Me.ReportButton.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
-        Me.ReportButton.BackColor = System.Drawing.Color.White
-        Me.ReportButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.ReportButton.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.ReportButton.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.ReportButton.FlatAppearance.BorderSize = 0
-        Me.ReportButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.ReportButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.ReportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.ReportButton.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ReportButton.ForeColor = System.Drawing.Color.Black
-        Me.ReportButton.Location = New System.Drawing.Point(121, 15)
-        Me.ReportButton.Name = "ReportButton"
-        Me.ReportButton.Size = New System.Drawing.Size(123, 23)
-        Me.ReportButton.TabIndex = 1
-        Me.ReportButton.Text = "Création d'un rapport"
-        Me.ReportButton.UseVisualStyleBackColor = False
         '
         'Button2
         '
@@ -109,12 +64,67 @@ Partial Class Form1
         Me.Button2.Text = "Liste des rapports"
         Me.Button2.UseVisualStyleBackColor = False
         '
+        'ReportButton
+        '
+        Me.ReportButton.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.ReportButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ReportButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ReportButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ReportButton.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.ReportButton.FlatAppearance.BorderSize = 0
+        Me.ReportButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ReportButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ReportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ReportButton.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReportButton.ForeColor = System.Drawing.Color.Black
+        Me.ReportButton.Location = New System.Drawing.Point(121, 15)
+        Me.ReportButton.Name = "ReportButton"
+        Me.ReportButton.Size = New System.Drawing.Size(123, 23)
+        Me.ReportButton.TabIndex = 1
+        Me.ReportButton.Text = "Création d'un rapport"
+        Me.ReportButton.UseVisualStyleBackColor = False
+        '
+        'logout
+        '
+        Me.logout.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.logout.BackColor = System.Drawing.Color.White
+        Me.logout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.logout.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.logout.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.logout.FlatAppearance.BorderSize = 0
+        Me.logout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.logout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.logout.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.logout.ForeColor = System.Drawing.Color.Black
+        Me.logout.Location = New System.Drawing.Point(764, 15)
+        Me.logout.Name = "logout"
+        Me.logout.Size = New System.Drawing.Size(95, 23)
+        Me.logout.TabIndex = 0
+        Me.logout.Text = "Déconnexion"
+        Me.logout.UseVisualStyleBackColor = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "images.jpg")
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel2.Location = New System.Drawing.Point(12, 89)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(872, 480)
+        Me.Panel2.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(896, 861)
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -128,4 +138,5 @@ Partial Class Form1
     Friend WithEvents ImageList1 As ImageList
     Friend WithEvents Button2 As Button
     Friend WithEvents ReportButton As Button
+    Friend WithEvents Panel2 As Panel
 End Class
