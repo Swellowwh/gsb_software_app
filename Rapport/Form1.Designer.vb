53faba341a1bf7ae8c9270a8d26f8570cfee838e
Skream20 <118281774+Skream20@users.noreply.github.com>
2024-10-18 11:00:55 +0200

    boutton

--- Rapport/Form1.Designer.vb
+++ Rapport/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,41 +20,112 @@ Partial Class Form1
     'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
     'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
     'Ne la modifiez pas à l'aide de l'éditeur de code.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
-        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.logout = New System.Windows.Forms.Button()
+        Me.ReportButton = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'CheckedListBox1
+        'Panel1
         '
-        Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Location = New System.Drawing.Point(163, 82)
-        Me.CheckedListBox1.Name = "CheckedListBox1"
-        Me.CheckedListBox1.Size = New System.Drawing.Size(470, 139)
-        Me.CheckedListBox1.TabIndex = 0
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.ReportButton)
+        Me.Panel1.Controls.Add(Me.logout)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(872, 53)
+        Me.Panel1.TabIndex = 0
         '
-        'CheckedListBox2
+        'ImageList1
         '
-        Me.CheckedListBox2.FormattingEnabled = True
-        Me.CheckedListBox2.Location = New System.Drawing.Point(344, 300)
-        Me.CheckedListBox2.Name = "CheckedListBox2"
-        Me.CheckedListBox2.Size = New System.Drawing.Size(120, 94)
-        Me.CheckedListBox2.TabIndex = 1
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "images.jpg")
+        '
+        'logout
+        '
+        Me.logout.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.logout.BackColor = System.Drawing.Color.White
+        Me.logout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.logout.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.logout.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.logout.FlatAppearance.BorderSize = 0
+        Me.logout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.logout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.logout.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.logout.ForeColor = System.Drawing.Color.Black
+        Me.logout.Location = New System.Drawing.Point(764, 15)
+        Me.logout.Name = "logout"
+        Me.logout.Size = New System.Drawing.Size(95, 23)
+        Me.logout.TabIndex = 0
+        Me.logout.Text = "Déconnexion"
+        Me.logout.UseVisualStyleBackColor = False
+        '
+        'ReportButton
+        '
+        Me.ReportButton.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.ReportButton.BackColor = System.Drawing.Color.White
+        Me.ReportButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ReportButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ReportButton.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.ReportButton.FlatAppearance.BorderSize = 0
+        Me.ReportButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ReportButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ReportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ReportButton.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReportButton.ForeColor = System.Drawing.Color.Black
+        Me.ReportButton.Location = New System.Drawing.Point(121, 15)
+        Me.ReportButton.Name = "ReportButton"
+        Me.ReportButton.Size = New System.Drawing.Size(123, 23)
+        Me.ReportButton.TabIndex = 1
+        Me.ReportButton.Text = "Création d'un rapport"
+        Me.ReportButton.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI Variable Display", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(252, 15)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Liste des rapports"
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1309, 598)
-        Me.Controls.Add(Me.CheckedListBox2)
-        Me.Controls.Add(Me.CheckedListBox1)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(896, 861)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents CheckedListBox1 As CheckedListBox
-    Friend WithEvents CheckedListBox2 As CheckedListBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents logout As Button
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ReportButton As Button
 End Class
