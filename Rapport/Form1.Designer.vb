3c82fd440d04fd75ea8fdb6a6f9f1946065e6c31
Skream20 <118281774+Skream20@users.noreply.github.com>
2024-10-18 09:46:21 +0200

    Mise à jour

--- Rapport/Form1.Designer.vb
+++ Rapport/Form1.Designer.vb
@@ -22,17 +22,28 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
         Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(163, 82)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(470, 139)
+        Me.CheckedListBox1.TabIndex = 0
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1309, 598)
+        Me.Controls.Add(Me.CheckedListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents CheckedListBox1 As CheckedListBox
 End Class
