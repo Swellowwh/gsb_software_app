517b9ba6be5c2932af33708e4008a247804f4242
Skream20 <118281774+Skream20@users.noreply.github.com>
2024-10-18 09:51:04 +0200

    tere

--- Rapport/Form1.Designer.vb
+++ Rapport/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
         Me.SuspendLayout()
         '
         'CheckedListBox1
@@ -33,11 +34,20 @@ Partial Class Form1
         Me.CheckedListBox1.Size = New System.Drawing.Size(470, 139)
         Me.CheckedListBox1.TabIndex = 0
         '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Location = New System.Drawing.Point(344, 300)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(120, 94)
+        Me.CheckedListBox2.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1309, 598)
+        Me.Controls.Add(Me.CheckedListBox2)
         Me.Controls.Add(Me.CheckedListBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -46,4 +56,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents CheckedListBox2 As CheckedListBox
 End Class
